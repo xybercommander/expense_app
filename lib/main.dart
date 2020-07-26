8524f620a5cce563d23620a3d77d7cbb3ba2dab8
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-26 20:20:23 +0530

    checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:expense_app/transaction.dart';
+import 'package:expense_app/models/transaction.dart';
+import 'package:expense_app/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,12 +19,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 200, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Weekly Groceries', amount: 100, date: DateTime.now()),
-  ];
 
   final titleController = new TextEditingController();
   final amountController = new TextEditingController();
@@ -48,68 +42,30 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             Card(
-                elevation: 5,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Title'),
-                        controller: titleController,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(labelText: 'Amount'),
-                        controller: amountController,
-                      ),
-                      FlatButton(
-                        child: Text("Add Transaction"),
-                        textColor: Colors.purple,
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
-                )),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '₹${tx.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: titleController,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: amountController,
+                    ),
+                    FlatButton(
+                      child: Text("Add Transaction"),
+                      textColor: Colors.purple,
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              )
+            ),
+            TransactionList()
           ],
         ));
   }
