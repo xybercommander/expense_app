2e0abf0baeca2f0f4d638c56679db7d7bcd9341a
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-26 20:27:23 +0530

    added new files

---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
